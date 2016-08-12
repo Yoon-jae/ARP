5633583d8bc4be38bd9ddce99c4f5e5c4ee9d019
Kim Min-Ho <minho1a@hanmail.net>
2016-08-12 14:03:33 +0900

    fixed ARPLayer.

--- ARP/ARPLayer.cpp
+++ ARP/ARPLayer.cpp
@@ -20,7 +20,15 @@ CARPLayer::~CARPLayer()
 
 void CARPLayer::ResetHeader()
 {
-
+	m_sHeader.arp_hardType = 0x0000;
+	m_sHeader.arp_protocolType = 0x0000;
+	m_sHeader.arp_hardLength = 0x00;
+	m_sHeader.arp_protocolLength = 0x00;
+	m_sHeader.arp_option = 0x0000;
+	memset(m_sHeader.arp_srcHardAddress.addrs, 0, 6);
+	memset(m_sHeader.arp_srcProtocolAddress.addrs, 0, 4);
+	memset(m_sHeader.arp_dstHardAddress.addrs, 0, 6);
+	memset(m_sHeader.arp_dstProtocolAddress.addrs, 0, 4);
 }
 
 BOOL CARPLayer::Send(unsigned char* ppayload, int nlength)
@@ -31,4 +39,54 @@ BOOL CARPLayer::Send(unsigned char* ppayload, int nlength)
 BOOL CARPLayer::Receive(unsigned char* ppayload)
 {
 	return false;
+}
+
+void CARPLayer::SetEnetSrcAddress(unsigned char *pAddress)
+{
+	memcpy(&m_sHeader.arp_srcHardAddress.addrs, pAddress, 6);
+}
+
+void CARPLayer::SetEnetDstAddress(unsigned char *pAddress)
+{
+	memcpy(&m_sHeader.arp_dstHardAddress.addrs, pAddress, 6);
+}
+
+void CARPLayer::SetSrcIPAddress(unsigned char* src_ip)
+{
+	memcpy(m_sHeader.arp_srcProtocolAddress.addrs, src_ip, 4);
+}
+
+void CARPLayer::SetDstIPAddress(unsigned char* dst_ip)
+{
+	memcpy(m_sHeader.arp_dstProtocolAddress.addrs, dst_ip, 4);
+}
+
+void CARPLayer::SetOption(unsigned int arp_option)
+{
+	m_sHeader.arp_option = arp_option;
+}
+
+unsigned char* CARPLayer::GetEnetSrcAddress()
+{
+	return m_sHeader.arp_srcHardAddress.addrs;
+}
+
+unsigned char* CARPLayer::GetEnetDstAddress()
+{
+	return m_sHeader.arp_dstHardAddress.addrs;
+}
+
+unsigned char* CARPLayer::GetSrcIPAddress()
+{
+	return m_sHeader.arp_srcProtocolAddress.addrs;
+}
+
+unsigned char* CARPLayer::GetDstIPAddress()
+{
+	return m_sHeader.arp_dstProtocolAddress.addrs;
+}
+
+unsigned int CARPLayer::GetOption()
+{
+	return m_sHeader.arp_option;
 }
--- ARP/ARPLayer.h
+++ ARP/ARPLayer.h
@@ -9,7 +9,8 @@ class CARPLayer
 {
 private:
 	inline void		ResetHeader( );
-	
+
+
 	// Cache table data structure
 
 
@@ -17,19 +18,66 @@ public:
 	CARPLayer( char* pName );
 	virtual ~CARPLayer();
 
+	void SetSrcIPAddress(unsigned char* src_ip);
+	void SetDstIPAddress(unsigned char* dst_ip);
+	void SetEnetSrcAddress(unsigned char* pAddress);
+	void SetEnetDstAddress(unsigned char* pAddress);
+	void SetOption(unsigned int arp_option);
+
+	unsigned char* GetSrcIPAddress();
+	unsigned char* GetDstIPAddress();
+	unsigned char* GetEnetSrcAddress();
+	unsigned char* GetEnetDstAddress();
+	unsigned int GetOption();
+
 	BOOL Send(unsigned char* ppayload, int nlength);
 	BOOL Receive(unsigned char* ppayload);
 
+	typedef struct _ETHERNET_ADDR
+	{
+		union {
+			struct { unsigned char e0, e1, e2, e3, e4, e5; } s_un_byte;
+			unsigned char s_ether_addr[6];
+		} S_un;
+
+		#define addr0 S_un.s_un_byte.e0
+		#define addr1 S_un.s_un_byte.e1
+		#define addr2 S_un.s_un_byte.e2
+		#define addr3 S_un.s_un_byte.e3
+		#define addr4 S_un.s_un_byte.e4
+		#define addr5 S_un.s_un_byte.e5
+
+		#define addrs  S_un.s_ether_addr
+
+	} ETHERNET_ADDR, *LPETHERNET_ADDR;
+
+	typedef struct _IP_ADDR
+	{
+		union {
+			struct { unsigned char e0, e1, e2, e3; } s_un_byte;
+			unsigned char s_ether_addr[4];
+		} S_un;
+
+		#define addr0 S_un.s_un_byte.e0
+		#define addr1 S_un.s_un_byte.e1
+		#define addr2 S_un.s_un_byte.e2
+		#define addr3 S_un.s_un_byte.e3
+
+		#define addrs  S_un.s_ether_addr
+
+	} IP_ADDR, *LPIP_ADDR;
+
+
 	typedef struct _ARPLayer_HEADER {
 		unsigned short arp_hardType;
-		unsigned short arp_protocol;
+		unsigned short arp_protocolType;
 		unsigned char arp_hardLength;
 		unsigned char arp_protocolLength;
 		unsigned short arp_option;
-		unsigned int arp_srcHardAddress;
-		unsigned int arp_srcProtocolAddress;
-		unsigned int arp_dstHardAddress;
-		unsigned int arp_dstHardProtocolAddress;
+		ETHERNET_ADDR arp_srcHardAddress;
+		IP_ADDR arp_srcProtocolAddress;
+		ETHERNET_ADDR arp_dstHardAddress;
+		IP_ADDR arp_dstProtocolAddress;
 	} ARPLayer_HEADER, *PARPLayer_HEADER;
 
 protected:
